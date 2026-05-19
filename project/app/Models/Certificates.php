1248bb68eda109ade8dafed0d18b354e6b0e4d38
[DaianaCardinaux] <[daianacardinau2@hotmail.com]>
2026-05-19 18:29:44 -0300

    log certificates

---
+++ project/app/Models/Certificates.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Certificates extends Model
+{
+    protected $fillable = ['participant_id', 'event_id', 'certificate_url', 'issued_at'];
+}
