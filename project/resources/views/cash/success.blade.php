a50acb0ffb67e91d0aaf5403e3f2e7d6ee5f9ea6
[DaianaCardinaux] <[daianacardinau2@hotmail.com]>
2026-06-16 18:50:00 -0300

    correos

--- project/resources/views/cash/success.blade.php
+++ project/resources/views/cash/success.blade.php
@@ -6,6 +6,16 @@
 
 <section class="form-wrapper">
     
+    <div class="container">
+        <div class="form-box">
+
+            <h1 class="form-heading">¡Gracias por completar el formulario!</h1>
+
+            <p class="success-message">Tu inscripción ha sido registrada correctamente. En breve recibirás un correo de confirmación con los detalles de tu participación.</p>
+
+        </div>
+        
+    </div>
 </section>
 
 @endsection
