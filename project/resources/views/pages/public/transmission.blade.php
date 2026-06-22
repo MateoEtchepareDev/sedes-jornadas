c3c7e9b331693d5501ec463c3a29ed32b1ec517f
[DaianaCardinaux] <[daianacardinau2@hotmail.com]>
2026-06-22 17:49:32 -0300

    codigo

--- project/resources/views/pages/public/transmission.blade.php
+++ project/resources/views/pages/public/transmission.blade.php
@@ -16,11 +16,25 @@
             </div>
 
             <h1 class="stream-title">
-                Transmisión de la Jornada {{ $event->name}}
+                Transmisión de la Jornada {{ $event->title}}
             </h1>
+
+            <iframe width="560" 
+            height="315" 
+            src="{{ $event->stream_url }}" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; 
+            autoplay; clipboard-write; 
+            encrypted-media; gyroscope; 
+            picture-in-picture; 
+            web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen></iframe>
 
             <p class="stream-subtitle">
                 Sigue la transmisión en vivo del evento en tiempo real.
+            </p>
                 <form action="/comments" method="POST">
                 @csrf
 
