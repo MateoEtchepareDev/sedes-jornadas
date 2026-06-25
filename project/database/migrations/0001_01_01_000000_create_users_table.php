0463b6466de544db6628085d0956ea321c392ac6
unknown <mateodemian@gmail.com>
2026-06-25 06:46:43 -0300

    migration fix

--- project/database/migrations/0001_01_01_000000_create_users_table.php
+++ project/database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('password_hash');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('access_code')->nullable()->unique();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
